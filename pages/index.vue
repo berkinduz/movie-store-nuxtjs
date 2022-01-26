b1c07943974ed0ec4d1f67a6bcc76acad12708eb
berkinduz <duzberkin@gmail.com>
2022-01-26 22:24:19 +0300

    Hero section added and imported to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <Hero />
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=7ebc825ac922c2ea86fa658f7c33f0bb&language=en-US&page=1`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+    },
+  },
 }
 </script>
